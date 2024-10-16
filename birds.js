37a06ede969490570e904a303cbb56b05d1be80c
Mohammad  Sartaj <sartajmohammad003@gmail.com>
2024-10-16 12:10:36 +0530

    Add files via upload

---
+++ birds.js
@@ -0,0 +1,18 @@
+const birdImages = [
+    {
+        name: "Sparrow",
+        image: "./asset/sparrow.jpg",
+        details: "The sparrow is a small, plump, brown and grey bird."
+    },
+    {
+        name: "Eagle",
+        image: "./asset/eagle.jpeg",
+        details: "Eagles are large birds of prey known for their keen sight."
+    },
+    {
+        name: "Parrot",
+        image: "./asset/parrate.jpeg",
+        details: "Parrots are colorful birds known for their ability to mimic sounds."
+    }
+    // Add more bird entries as needed
+];
